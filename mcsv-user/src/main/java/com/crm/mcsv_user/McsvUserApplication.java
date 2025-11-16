19ea8516468770b4ae915dacc545513bd614aac8
Guillermo Diaz <guillermoe.diazq@gmail.com>
2025-11-16 01:24:36 -0300

    feat wip backend mcsv-auth with user in mcsv-user

--- mcsv-user/src/main/java/com/crm/mcsv_user/McsvUserApplication.java
+++ mcsv-user/src/main/java/com/crm/mcsv_user/McsvUserApplication.java
@@ -2,8 +2,14 @@ package com.crm.mcsv_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableJpaRepositories
 public class McsvUserApplication {
 
 	public static void main(String[] args) {
