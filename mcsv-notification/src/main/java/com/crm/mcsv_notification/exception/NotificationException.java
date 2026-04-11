a9663ee20cdd71975c5a52d30dfa03b396cc6397
Guillermo Diaz Q. <guillermoe.diazq@gmail.com>
2026-04-11 02:18:17 -0400

    se elimina mcsv-notificaciones y pasa a arquitectura serverless

--- mcsv-notification/src/main/java/com/crm/mcsv_notification/exception/NotificationException.java
+++
@@ -1,7 +0,0 @@
-package com.crm.mcsv_notification.exception;
-
-public class NotificationException extends RuntimeException {
-    public NotificationException(String message) {
-        super(message);
-    }
-}
