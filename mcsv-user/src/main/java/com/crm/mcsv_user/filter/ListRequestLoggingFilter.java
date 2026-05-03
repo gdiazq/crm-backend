c3d7976ef88933c57dac09294bebce356c3f6f14
Guillermo Diaz Q. <guillermoe.diazq@gmail.com>
2026-05-02 20:59:24 -0400

    se realiza un log para vista list despues se eliminara

---
+++ mcsv-user/src/main/java/com/crm/mcsv_user/filter/ListRequestLoggingFilter.java
@@ -0,0 +1,37 @@
+package com.crm.mcsv_user.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class ListRequestLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        if (!"GET".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
+
+        String path = request.getRequestURI();
+        return !(path.endsWith("/paged") || path.contains("/select/"));
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        log.info("List request received: path={}, query={}, userId={}",
+                request.getRequestURI(),
+                request.getQueryString(),
+                request.getHeader("x-user-id"));
+
+        filterChain.doFilter(request, response);
+    }
+}
