5a115d8a54bc8ed741855a07a5a0f00470e105a6
Guillermo Diaz Q. <guillermoe.diazq@gmail.com>
2026-05-19 13:18:46 -0400

    se saca circuit breaker de mcsv-auth

--- mcsv-auth/src/main/java/com/crm/mcsv_auth/client/UserClient.java
+++ mcsv-auth/src/main/java/com/crm/mcsv_auth/client/UserClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient(name = "mcsv-user", contextId = "authUserClient", fallback = UserClientFallback.class)
+@FeignClient(name = "mcsv-user", contextId = "authUserClient")
 public interface UserClient {
 
     @GetMapping("/detail/username/{username}")
