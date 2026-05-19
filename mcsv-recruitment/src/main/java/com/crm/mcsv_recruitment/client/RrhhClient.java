fbeaf2be1f8d2a0de3c664f6331c87ac37ccbfb3
Guillermo Diaz Q. <guillermoe.diazq@gmail.com>
2026-05-19 13:23:30 -0400

    se saca circuit breaker de mcsv-recruitment

--- mcsv-recruitment/src/main/java/com/crm/mcsv_recruitment/client/RrhhClient.java
+++ mcsv-recruitment/src/main/java/com/crm/mcsv_recruitment/client/RrhhClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "mcsv-rrhh", fallback = com.crm.mcsv_recruitment.client.fallback.RrhhClientFallback.class)
+@FeignClient(name = "mcsv-rrhh")
 public interface RrhhClient {
 
     @GetMapping("/select/job-titles/{id}")
