081fd8ee04a2aa4bcabff97fa850e7a042ed9e16
Guillermo Diaz Q. <guillermoe.diazq@gmail.com>
2026-05-23 14:39:35 -0400

    se crea select de active/inactive en common

---
+++ mcsv-common/src/main/java/com/crm/common/controller/ActiveInactiveSelectController.java
@@ -0,0 +1,23 @@
+package com.crm.common.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/select/active-inactive")
+public class ActiveInactiveSelectController {
+
+    @GetMapping
+    public ResponseEntity<List<Item>> getAll() {
+        return ResponseEntity.ok(List.of(
+                new Item(1L, true),
+                new Item(2L, false)
+        ));
+    }
+
+    public record Item(Long id, Boolean name) {}
+}
