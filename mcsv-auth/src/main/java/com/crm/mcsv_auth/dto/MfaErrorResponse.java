ddb14b725ac3d64a21573c0206ea7d19cddc9955
Guillermo Diaz Q. <guillermoe.diazq@gmail.com>
2026-05-24 16:29:48 -0400

    se mueve errorResponse a su propio dto

---
+++ mcsv-auth/src/main/java/com/crm/mcsv_auth/dto/MfaErrorResponse.java
@@ -0,0 +1,22 @@
+package com.crm.mcsv_auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MfaErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private boolean mfaRequired;
+    private String path;
+}
