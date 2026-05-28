edd3c0d8b5a6d543d1ad2a059d9149b71bb256b2
Guillermo Diaz Q. <guillermoe.diazq@gmail.com>
2026-05-28 18:32:18 -0400

    se mueve catalogo de meal

--- mcsv-rrhh/src/main/java/com/crm/mcsv_rrhh/repository/mealtype/MealTypeRepository.java
+++ mcsv-rrhh/src/main/java/com/crm/mcsv_rrhh/repository/mealtype/MealTypeRepository.java
@@ -1,6 +1,6 @@
-package com.crm.mcsv_rrhh.repository;
+package com.crm.mcsv_rrhh.repository.mealtype;
 
-import com.crm.mcsv_rrhh.entity.MealType;
+import com.crm.mcsv_rrhh.entity.mealtype.MealType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
