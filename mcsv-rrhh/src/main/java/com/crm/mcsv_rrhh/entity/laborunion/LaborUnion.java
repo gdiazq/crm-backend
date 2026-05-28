9dc30814a7ba34466824730aa57c0f187074dddc
Guillermo Diaz Q. <guillermoe.diazq@gmail.com>
2026-05-28 01:36:02 -0400

    se mueve catalogo de laborunion

--- mcsv-rrhh/src/main/java/com/crm/mcsv_rrhh/entity/laborunion/LaborUnion.java
+++ mcsv-rrhh/src/main/java/com/crm/mcsv_rrhh/entity/laborunion/LaborUnion.java
@@ -1,4 +1,4 @@
-package com.crm.mcsv_rrhh.entity;
+package com.crm.mcsv_rrhh.entity.laborunion;
 
 import jakarta.persistence.*;
 import lombok.*;
--- mcsv-rrhh/src/main/java/com/crm/mcsv_rrhh/repository/laborunion/LaborUnionRepository.java
+++ mcsv-rrhh/src/main/java/com/crm/mcsv_rrhh/repository/laborunion/LaborUnionRepository.java
@@ -1,6 +1,6 @@
-package com.crm.mcsv_rrhh.repository;
+package com.crm.mcsv_rrhh.repository.laborunion;
 
-import com.crm.mcsv_rrhh.entity.LaborUnion;
+import com.crm.mcsv_rrhh.entity.laborunion.LaborUnion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
